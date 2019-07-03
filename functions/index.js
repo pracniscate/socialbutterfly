b7f23e5083e9f949cbc430a3533886b46437024f
Desmond Giddens <lorena.giddens@outlook.com>
2019-07-02 20:57:21 -0400

    create a function to collect posts

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,16 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.getShouts = functions.https.onRequest((req, res) => {
+    // need access to the database
+    admin.firestore().collection('shouts').get()
+        .then(data => {
+            let shouts = [];
+            data.forEach(doc => {
+                shouts.push(doc.data());
+            });
+            return res.json(shouts);
+        })
+        .catch(err => console.error(err));
+})
