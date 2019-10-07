94e8d4938db3d8f0f26417342b0e9c891dd8ca78
pracniscate <lorena.giddens@outlook.com>
2019-10-07 09:18:30 -0400

    create db schema for reference

---
+++ dbschema.js
@@ -0,0 +1,15 @@
+// this file is just for reference
+
+let db = {
+  screams: [
+    {
+      // userHandle identifies the owner of the shout
+      userHandle: 'user',
+      body: 'this is the shout body',
+      createdAt: '2019-10-07T13:09:23.105Z',
+      // minimize the number of reads when user sends a request
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
