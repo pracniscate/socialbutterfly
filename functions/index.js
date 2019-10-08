af00f4507ec56f3e7249698c3da37f91678d4f74
pracniscate <lorena.giddens@outlook.com>
2019-10-07 22:12:48 -0400

    initialize firebase app

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,22 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+// initialize firebase app so that users can authenticate through it
+const firebaseConfig = {
+    apiKey: "AIzaSyBAsa3Jf3oj2hq1e3XYlMaeT-4v60yQPXQ",
+    authDomain: "socialbutterfly-e5ca3.firebaseapp.com",
+    databaseURL: "https://socialbutterfly-e5ca3.firebaseio.com",
+    projectId: "socialbutterfly-e5ca3",
+    storageBucket: "socialbutterfly-e5ca3.appspot.com",
+    messagingSenderId: "480914195099",
+    appId: "1:480914195099:web:365653109b41a2b8b97c3e"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
 
 app.get('/shouts', (req, res) => {
     admin
